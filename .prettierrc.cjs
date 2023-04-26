665a3d1bb54575e21c08d7f1c332fd9beab4e949
Roman-Kohutiak <rkohu@softserveinc.com>
2023-04-26 07:55:27 +0300

    add jest, rts lib, eslint, prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
+  organizeImportsSkipDestructiveCodeActions: true,
+  pluginSearchDirs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  jsxSingleQuote: true,
+  tabWidth: 2,
+};
